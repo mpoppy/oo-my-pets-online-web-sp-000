ee50e99cbd116c168414f9c9476253b2aeacdaa0
Marc Poppy <poppy.marc@gmail.com>
2019-06-30 20:35:36 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
 class Cat
-  # code goes here
+
+  attr_accessor :mood
+  attr_reader :name, :owner
+
+  @@all = []
+
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def owner=(owner) #belongs to owner
+    @owner = owner
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+  end
+
 end
